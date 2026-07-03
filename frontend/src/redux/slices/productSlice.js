7655800a36b1b34810382ffcba279dd2744f4f48
Sona Binu <sonavbinu567@gmail.com>
2026-07-03 17:13:06 +0530

    Add productslice

--- frontend/src/redux/slices/productSlice.js
+++ frontend/src/redux/slices/productSlice.js
@@ -1,17 +1,87 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  products: JSON.parse(localStorage.getItem("products")) || [],
+  categories: [
+    {
+      id: 1,
+      name: "Fruits & Vegetables",
+      products: [],
+    },
+    {
+      id: 2,
+      name: "Dairy,Bread & Eggs",
+      products: [],
+    },
+    {
+      id: 3,
+      name: "Snacks & Biscuits",
+      products: [],
+    },
+    {
+      id: 4,
+      name: "Atta, Dal & Rice",
+      products: [],
+    },
+    {
+      id: 5,
+      name: "Dry Fruits & Masala",
+      products: [],
+    },
+    {
+      id: 6,
+      name: "Tea,Coffee & More",
+      products: [],
+    },
+    {
+      id: 7,
+      name: "Chocolate & Desserts",
+      products: [],
+    },
+  ],
 };
 
-const products = createSlice({
+const productSlice = createSlice({
   name: "products",
   initialState,
   reducers: {
     addProduct: (state, action) => {
-      state.products.push(action.payload);
+      const { categoryId, product } = action.payload;
 
-      localStorage.setItem("products", JSON.stringify(state.products));
+      const category = state.categories.find((cat) => cat.id === categoryId);
+      if (category) {
+        category.products.push({
+          id: Date.now(),
+          ...product,
+        });
+      }
+    },
+    deleteProduct: (state, action) => {
+      const { categoryId, productId } = action.payload;
+
+      const category = state.categories.find((cat) => cat.id === categoryId);
+    },
+
+    updateProduct: (state, action) => {
+      const { categoryId, productId, updateProduct } = action.payload;
+      const category = state.categories.find((cat) => cat.id === categoryId);
+
+      if (category) {
+        const index = category.products.findIndex(
+          (product) => product.id === productId,
+        );
+
+        if (index !== -1) {
+          category.products[index] = {
+            ...category.products[index],
+            ...updateProduct,
+          };
+        }
+      }
     },
   },
 });
+
+export const { addProduct, deleteProduct, updateProduct } =
+  productSlice.actions;
+
+export default productSlice.reducer;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/themeSlice";
 import orderReducer from "./slices/orderSlice";
+import productReducer from "./slices/productSlice";
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
     orders: orderReducer,
+    products: productReducer,
   },
 });
 
