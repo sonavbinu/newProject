cb6828713d2cda990f07cd73b8fb7259c48b0253
Sona Binu <sonavbinu567@gmail.com>
2026-07-07 11:57:28 +0530

    add

--- frontend/src/Pages/Dashboard/MyProducts/MyProducts.jsx
+++ frontend/src/Pages/Dashboard/MyProducts/MyProducts.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import { addProduct } from "../../../redux/slices/productSlice";
+import { useTranslation } from "react-i18next";
 
 const MyProducts = () => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -24,11 +26,14 @@ const MyProducts = () => {
   return (
     <div>
       <div className="flex flex-col gap-2">
-        <h2 className="text-xl sm:text-lg font-bold">Product Listing</h2>
+        <h2 className="text-xl sm:text-lg font-bold">
+          {" "}
+          {t("myProducts.title")}
+        </h2>
         <div className="flex gap-4 justify-between">
           <input
             type="text"
-            placeholder="Search for products or category"
+            placeholder={t("myProducts.searchPlaceholder")}
             className="border border-gray-300 rounded-xl px-2 py-2 focus:ring-2 focus:ring-[var(--primary-color)] 
             outline-none w-[80%]"
           />
@@ -36,7 +41,7 @@ const MyProducts = () => {
             onClick={() => navigate("/add-product")}
             className="bg-[var(--primary-color)]  text-white rounded-xl px-2 hover:opacity-90 cursor-pointer"
           >
-            Add Product
+            {t("myProducts.addProduct")}
           </button>
         </div>
         <Outlet />
