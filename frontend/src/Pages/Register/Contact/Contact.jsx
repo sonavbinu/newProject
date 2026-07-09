5999757b70c479fb3ede0ee56d0e3ce155fef694
Sona Binu <sonavbinu567@gmail.com>
2026-07-09 10:55:26 +0530

    Add few changes

--- frontend/src/Pages/Register/Contact/Contact.jsx
+++ frontend/src/Pages/Register/Contact/Contact.jsx
@@ -5,24 +5,24 @@ import { useNavigate } from "react-router-dom";
 const Contact = () => {
   const navigate = useNavigate();
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen bg-gray-50 w-full flex flex-col ">
       <div className="py-6 text-center">
         {" "}
         <h1 className="text-4xl font-bold text-[var(--primary-color)]">
           ORIGIN{" "}
         </h1>
-        <img src={handShake} alt="" />
+        <img src={handShake} alt="" className="w-full" />
         <div className="flex flex-col items-center justify-center gap-4  mt-6 px-6 lg:px-20 py-10 rounded">
           <h1 className="text-2xl font-bold">Contact Form</h1>
           <p className="text-gray-400 text-sm">
             Fill the contact form, One of our <br />
             executives will reach out to you shortly
           </p>
-          <form className="flex flex-col gap-4 w-[50%]">
+          <form className="flex flex-col gap-4 max-w-md mx-auto w-full">
             <input
               type="text"
               placeholder="Owner Name"
-              className="p-3 border border-gray-300 rounded focus:ring-2 focus:ring-[var(--primary-color)]  focus:outline-none"
+              className="p-3 border  border-gray-300 rounded focus:ring-2 focus:ring-[var(--primary-color)]  focus:outline-none"
             />
             <input
               type="text"
