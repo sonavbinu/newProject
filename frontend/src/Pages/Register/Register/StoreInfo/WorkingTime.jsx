eeadeaa1a9bf514d597d113f381d63645aec0b1b
Sona Binu <sonavbinu567@gmail.com>
2026-07-13 16:05:49 +0530

    add

--- frontend/src/Pages/Register/Register/StoreInfo/WorkingTime.jsx
+++ frontend/src/Pages/Register/Register/StoreInfo/WorkingTime.jsx
@@ -1,40 +1,55 @@
 import React from "react";
+import { Clock3, Info } from "lucide-react";
 
 const WorkingTime = () => {
   return (
-    <div className="bg-white border border-gray-200 rounded-xl shadow-sm p-8">
-      <h2 className="text-2xl font-bold mb-6">Working Time</h2>
-
+    <div className="bg-white border border-gray-200 rounded-2xl shadow-md p-8">
+      <div className="flex items-center  gap-3 mb-6">
+        <div className="w-12 h-12 rounded-full bg-[var(--primary-light)] flex items-center justify-center">
+          <Clock3 size={22} className="text-[var(--primary-color)]" />
+        </div>{" "}
+      </div>{" "}
+      <h2 className="text-2xl  text-gray-900 font-bold mb-6">Working Time</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {/* Opening Time */}
-        <div className="flex flex-col gap-2">
-          <label className="text-sm font-medium text-gray-700">
+        <div>
+          <label className=" block mb-2 text-sm font-medium text-gray-700">
             Opening Time
           </label>
 
           <input
             type="time"
-            className="w-full border border-gray-300 rounded-lg px-4 py-3
+            className="w-full border border-gray-300 rounded-xl px-4 py-3
             focus:outline-none focus:ring-2
             focus:ring-[var(--primary-color)]
-            focus:border-[var(--primary-color)]"
+            focus:border-transparent
+            transition
+            "
           />
         </div>
 
         {/* Closing Time */}
-        <div className="flex flex-col gap-2">
-          <label className="text-sm font-medium text-gray-700">
+        <div>
+          <label className="text-sm block mb-2 font-medium text-gray-700">
             Closing Time
           </label>
 
           <input
             type="time"
-            className="w-full border border-gray-300 rounded-lg px-4 py-3
+            className="w-full rounded-xl border border-gray-300 rounded-lg px-4 py-3
             focus:outline-none focus:ring-2
             focus:ring-[var(--primary-color)]
-            focus:border-[var(--primary-color)]"
+            focus:border-transparent transition"
           />
         </div>
+      </div>
+      <div className="mt-6 flex items-start gap-3 rounded-xl border border-blue-200 bg-blue-50 p-4">
+        <Info size={20} className="text-blue-600 mt-0.5 flex-shrink-0" />
+
+        <p className="text-sm text-blue-700">
+          Customers will see these timings while placing orders. Please make
+          sure they match your actual business hours.{" "}
+        </p>
       </div>
     </div>
   );
