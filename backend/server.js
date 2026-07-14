2eb64e10c2c65f30916f6265b8e97c74951b73fc
Sona Binu <sonavbinu567@gmail.com>
2026-07-14 11:12:36 +0530

    connected mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,21 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
 app.get("/", (req, res) => {
-  res.send("Backend Running");
+  res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
